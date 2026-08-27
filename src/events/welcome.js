3ab6f55d0a797c4e8a150bf3c8d105b9097f4157
teamluan <dickluan12@gmail.com>
2026-08-27 11:50:56 +0200

    Create welcome event

---
+++ src/events/welcome.js
@@ -0,0 +1,9 @@
+const { welcomeEmbed } = require('../utils/embeds');
+
+module.exports = member => {
+  const config = require('../config').load();
+  if (!config.welcome.enabled || !config.welcome.channelId) return;
+  const channel = member.guild.channels.cache.get(config.welcome.channelId);
+  if (!channel?.isTextBased()) return;
+  return channel.send({ embeds: [welcomeEmbed(config.welcome.message, member)] }).catch(() => {});
+};
